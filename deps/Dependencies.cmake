09e3f96064b130fe6d98d5f56ba4821a3147e2dd
Michael Beckemeyer <m.beckemeyer@gmx.net>
2017-01-24 21:40:44 +0100

    Reorganize cmake dependencies.

--- deps/Dependencies.cmake
+++ deps/Dependencies.cmake
@@ -1,8 +1,11 @@
 include(ExternalProject)
 
+set(DEPS_SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}")
+set(DEPS_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/deps")
+
 ### GSL
 add_library(gsl INTERFACE)
-target_include_directories(gsl SYSTEM INTERFACE "GSL")
+target_include_directories(gsl SYSTEM INTERFACE "${DEPS_SOURCE_DIR}/GSL")
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     #target_compile_definitions(gsl INTERFACE GSL_THROW_ON_CONTRACT_VIOLATION)
     target_compile_definitions(gsl INTERFACE GSL_TERMINATE_ON_CONTRACT_VIOLATION)
@@ -11,12 +14,12 @@ else()
 endif()
 
 add_library(catch INTERFACE)
-target_include_directories(catch INTERFACE "catch")
+target_include_directories(catch INTERFACE "${DEPS_SOURCE_DIR}/catch")
 
 ### fmt
 ExternalProject_Add(project_fmt
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/fmt-3.0.0"
-    PREFIX     "${CMAKE_CURRENT_BINARY_DIR}/fmt-3.0.0"
+    SOURCE_DIR "${DEPS_SOURCE_DIR}/fmt-3.0.0"
+    PREFIX     "${DEPS_BINARY_DIR}/fmt-3.0.0"
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -DFMT_DOC=0
 )
 
@@ -28,8 +31,8 @@ target_link_libraries(fmt INTERFACE "${INSTALL_DIR}/lib/libfmt.a")
 
 ## tpie
 ExternalProject_add(project_tpie
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/tpie"
-    PREFIX     "${CMAKE_CURRENT_BINARY_DIR}/tpie"
+    SOURCE_DIR "${DEPS_SOURCE_DIR}/tpie"
+    PREFIX     "${DEPS_BINARY_DIR}/tpie"
     CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 )
 
@@ -49,3 +52,11 @@ add_library(boost INTERFACE)
 target_include_directories(boost SYSTEM INTERFACE "${Boost_INCLUDE_DIRS}")
 target_link_libraries(boost INTERFACE "${Boost_LIBRARIES}")
 
+### Qt5. Use Qt5::Core etc as link targets.
+find_package(Qt5 REQUIRED COMPONENTS Core Gui Widgets OpenGL)
+
+### openscenegraph
+find_package(OpenSceneGraph REQUIRED COMPONENTS osgDB osgGA osgUtil osgViewer)
+add_library(osg INTERFACE)
+target_include_directories(osg SYSTEM INTERFACE "${OPENSCENEGRAPH_INCLUDE_DIRS}")
+target_link_libraries(osg INTERFACE "${OPENSCENEGRAPH_LIBRARIES}")
