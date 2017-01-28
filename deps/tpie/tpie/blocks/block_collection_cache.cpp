594132ac4396c3cfb2492a72b67a1f32dd8cea18
Michael Beckemeyer <m.beckemeyer@gmx.net>
2017-01-28 12:29:57 +0100

    Respect cache size when allocating free blocks.

--- deps/tpie/tpie/blocks/block_collection_cache.cpp
+++ deps/tpie/tpie/blocks/block_collection_cache.cpp
@@ -45,6 +45,7 @@ block_collection_cache::~block_collection_cache() {
 block_handle block_collection_cache::get_free_block() {
 	block_handle h = m_collection.get_free_block();
 	block * cache_b = tpie_new<block>(m_blockSize);
+    prepare_cache();
 	add_to_cache(h, cache_b, true);
 	return h;
 }
@@ -65,7 +66,7 @@ void block_collection_cache::free_block(block_handle handle) {
 	m_collection.free_block(handle);
 }
 
-void block_collection_cache::prepare_cache() {
+void block_collection_cache::prepare_cache() {    
 	if(m_curSize < m_maxSize)
 		return;
 
@@ -82,7 +83,9 @@ void block_collection_cache::prepare_cache() {
 }
 
 void block_collection_cache::add_to_cache(block_handle handle, block * b, bool dirty) {
-	m_blockList.push_back(handle);
+    tp_assert(m_curSize < m_maxSize, "must not be full");
+
+    m_blockList.push_back(handle);
 	block_list_t::iterator list_pos = m_blockList.end();
 	--list_pos;
 
