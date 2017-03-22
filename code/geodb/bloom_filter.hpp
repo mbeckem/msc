6d7e055e7f6994a9abfb6ee28a56d05cb8d20752
Michael Beckemeyer <m.beckemeyer@gmx.net>
2017-03-22 17:34:08 +0100

    Implement bloom filters.

---
+++ code/geodb/bloom_filter.hpp
@@ -0,0 +1,220 @@
+#ifndef GEODB_BLOOM_FILTER_HPP
+#define GEODB_BLOOM_FILTER_HPP
+
+#include "geodb/common.hpp"
+#include "geodb/type_traits.hpp"
+
+#include <boost/math/constants/constants.hpp>
+#include <boost/range/functions.hpp>
+#include <boost/range/metafunctions.hpp>
+
+#include <array>
+#include <ostream>
+
+namespace geodb {
+
+std::array<u64, 2> murmur3(const u8* data, size_t size);
+
+template<typename T>
+std::array<u64, 2> murmur3(const T& value) {
+    static_assert(std::is_trivially_copyable<T>::value, "");
+    return murmur3(reinterpret_cast<const u8*>(std::addressof(value)), sizeof(value));
+}
+
+/// A basic bloom filter.
+///
+/// This class is trivially copyable and thus serializable by the TPIE IO classes.
+/// It takes approximately `ceil(bits / 8)` bytes of storage.
+template<typename T, u32 bits>
+class bloom_filter {
+    static_assert(std::is_trivially_copyable<T>::value,
+                  "The type must be trivially copyable, since its raw data "
+                  "is being hashed.");
+
+private:
+    /// Returns the number of uint64_t fields required to store the
+    /// requested number of bits (they are 64 bits each).
+    static constexpr u32 fields() {
+        return (bits + 63) / 64;
+    }
+
+    /// The number of different hash values computed
+    /// for every value.
+    static constexpr u32 hashes() {
+        return 5; // TODO
+    }
+
+    using data_t = std::array<u64, fields()>;
+
+    using hashes_t = std::array<u64, hashes()>;
+
+public:
+    using value_type = T;
+
+public:
+    /// Returns the approximate error rate when inserting the given number of elements
+    /// into a bloom filter with the current number of bits.
+    static double error_rate(u64 elements) {
+        double k = hashes();
+        double n = elements;
+        double m = bits;
+        return std::pow(1.0 - std::pow(1.0 - 1.0/m, k * n), k);
+    }
+
+    /// Computes the union of the given range of bloom filters.
+    /// They must all be of this type.
+    template<typename BloomFilters>
+    static bloom_filter set_union(const BloomFilters& range) {
+        bloom_filter result;
+        for (const bloom_filter& filter : range) {
+            for (u32 i = 0; i < fields(); ++i) {
+                result.m_data[i] |= filter.m_data[i];
+            }
+        }
+        return result;
+    }
+
+    /// Computes the intersection of the given range of bloom filters.
+    /// They must all be of this type.
+    template<typename BloomFilters>
+    static bloom_filter set_intersection(const BloomFilters& range) {
+        using const_iter = typename boost::range_const_iterator<const BloomFilters>::type;
+        using value_type = typename boost::range_value<const BloomFilters>::type;
+
+        static_assert(std::is_same<value_type, bloom_filter>::value,
+                      "The range must contain bloom filter values.");
+
+        const_iter first = boost::const_begin(range);
+        const_iter last = boost::const_end(range);
+        if (first == last) {
+            return bloom_filter();
+        }
+
+        bloom_filter result = *first;
+        ++first;
+        for (; first != last; ++first) {
+            const bloom_filter& filter = *first;
+
+            for (u32 i = 0; i < fields(); ++i) {
+                result.m_data[i] &= filter.m_data[i];
+            }
+        }
+        return result;
+    }
+
+public:
+    bloom_filter() = default;
+
+    /// Inserts the value into the bloom filter.
+    /// \post `contains(value) == true`.
+    void insert(const value_type& value) {
+        for (u64 hash : compute_hashes(value)) {
+            set(index(hash));
+        }
+    }
+
+    /// Returns true if the value is stored in this instance.
+    ///
+    /// This might be a false positive, but can never be
+    /// a false negative.
+    /// In other words, returning true means "probably".
+    bool contains(const value_type& value) const {
+        for (u64 hash : compute_hashes(value)) {
+            if (!test(index(hash))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// Returns the union of `*this` and `other`.
+    bloom_filter union_with(const bloom_filter& other) const {
+        bloom_filter result = *this;
+        for (u32 i = 0; i < fields(); ++i) {
+            result.m_data[i] |= other.m_data[i];
+        }
+        return result;
+    }
+
+    /// Returns the intersection of `*this` and `other`.
+    bloom_filter intersection_with(const bloom_filter& other) const {
+        bloom_filter result = *this;
+        for (u32 i = 0; i < fields(); ++i) {
+            result.m_data[i] &= other.m_data[i];
+        }
+        return result;
+    }
+
+private:
+    /// Returns the bit index for the given hash.
+    u32 index(u64 hash) const {
+        return hash % bits;
+    }
+
+    /// Computes the hashes for the given value by computing the 128 bit murmur hash.
+    /// Then treats both 64 bit components of the murmur hash as independent hash functions
+    /// (they're uniformly distributed, so this should be OK) and uses them to emulate N
+    /// hash functions.
+    ///
+    /// For the last part, see "Building a better Bloom Filter" by Adam Kirsch and Michael Mitzenmacher.
+    hashes_t compute_hashes(const value_type& value) const {
+        std::array<u64, 2> murmur = murmur3(value);
+
+        hashes_t result;
+        for (u32 i = 0; i < hashes(); ++i) {
+            result[i] = murmur[0] + i * murmur[1];
+        }
+        return result;
+    }
+
+    /// Sets the bit with the given index to "1".
+    void set(u32 index) {
+        geodb_assert(index < bits, "bit index out of bounds");
+
+        u32 element_index = index / 64;
+        u32 bit_index = index % 64;
+        m_data[element_index] |= (1 << bit_index);
+    }
+
+    /// Returns true if the bit with the given index is set.
+    bool test(u32 index) const {
+        geodb_assert(index < bits, "bit index out of bounds");
+
+        u32 element_index = index / 64;
+        u32 bit_index = index % 64;
+        return m_data[element_index] & (1 << bit_index);
+    }
+
+private:
+    friend bool operator==(const bloom_filter& a, const bloom_filter& b) {
+        for (u32 i = 0; i < fields(); ++i) {
+            if (a.m_data[i] != b.m_data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    friend bool operator!=(const bloom_filter& a, const bloom_filter& b) {
+        for (u32 i = 0; i < fields(); ++i) {
+            if (a.m_data[i] == b.m_data[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    friend std::ostream& operator<<(std::ostream& o, const bloom_filter& filter) {
+        for (u32 i = 0; i < bits; ++i) {
+            o << (filter.test(i) ? '1' : '0');
+        }
+        return o;
+    }
+
+private:
+    data_t m_data{};
+};
+
+} // namespace geodb
+
+#endif // GEODB_BLOOM_FILTER_HPP
---
+++ code/test/bloom_filter.cpp
@@ -0,0 +1,81 @@
+#include <catch.hpp>
+
+#include "geodb/bloom_filter.hpp"
+
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <vector>
+
+using namespace geodb;
+
+using bloom_filter_t = bloom_filter<u64, 128>;
+
+TEST_CASE("insert", "[bloom filter]") {
+    std::vector<u64> numbers{
+        1, 3, 6, 17, 65, 1346, 12357, 99344,
+        1345165, 356367ULL, 1341636466485ULL,
+        134163646648123125ULL,
+    };
+
+    bloom_filter_t bloom;
+    for (u64 num : numbers) {
+        bloom.insert(num);
+    }
+
+    for (u64 num : numbers) {
+        REQUIRE(bloom.contains(num));
+    }
+}
+
+TEST_CASE("intersection and union", "[bloom filter]") {
+    std::vector<u64> numbers_a{
+        1, 3, 6, 17, 65, 1346, 12357, 99344,
+        1345165,
+        356367ULL,
+        954547818ULL,
+        1341636466485ULL,
+        13416364664812312ULL,
+    };
+    std::vector<u64> numbers_b{
+        3, 12, 65, 188, 1346, 64555, 1345165, 3564657,
+        954547818ULL,
+        12344656348ULL,
+        1542895732490859ULL,
+        13416364664812312ULL,
+        3094290433892905685ULL,
+    };
+    std::vector<u64> numbers_intersection;
+    std::set_intersection(numbers_a.begin(), numbers_a.end(),
+                          numbers_b.begin(), numbers_b.end(),
+                          std::back_inserter(numbers_intersection));
+
+    std::vector<u64> numbers_union;
+    std::set_union(numbers_a.begin(), numbers_a.end(),
+                   numbers_b.begin(), numbers_b.end(),
+                   std::back_inserter(numbers_union));
+
+    bloom_filter_t filter_a, filter_b;
+    for (u64 a : numbers_a) {
+        filter_a.insert(a);
+    }
+
+    for (u64 b : numbers_b) {
+        filter_b.insert(b);
+    }
+
+    bloom_filter_t filter_intersection = filter_a.intersection_with(filter_b);
+    REQUIRE(filter_intersection == bloom_filter_t::set_intersection(
+                std::vector<bloom_filter_t>{filter_a, filter_b}));
+
+    for (u64 i : numbers_intersection) {
+        REQUIRE(filter_intersection.contains(i));
+    }
+
+    bloom_filter_t filter_union = filter_a.union_with(filter_b);
+    REQUIRE(filter_union == bloom_filter_t::set_union(
+                std::vector<bloom_filter_t>{filter_a, filter_b}));
+    for (u64 u : numbers_union) {
+        REQUIRE(filter_union.contains(u));
+    }
+}
